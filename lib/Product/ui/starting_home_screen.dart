371e65fa26b8001e11cb8482e8f8ffdf9cdee85d
amitpaju0001 <amitpaju0001@gmail.com>
2024-07-24 16:39:32 +0530

    created normal home screen ui using list view builder

---
+++ lib/Product/ui/starting_home_screen.dart
@@ -0,0 +1,47 @@
+
+import 'package:buy_smart_admin/Product/ui/product_screen.dart';
+import 'package:flutter/material.dart';
+
+class StartingHomeScreen extends StatefulWidget {
+  const StartingHomeScreen({super.key});
+  @override
+  State<StartingHomeScreen> createState() => _StartingHomeScreenState();
+}
+class _StartingHomeScreenState extends State<StartingHomeScreen> {
+  final List<Widget> screens = [
+     ProductScreen(),
+     Container(),
+
+  ];
+  int currentTab = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: currentTab,
+        children: screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentTab,
+        onTap: (int index) {
+          setState(() {
+            currentTab = index;
+          });
+        },
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Account',
+          ),
+        ],
+      ),
+    );
+  }
+}
