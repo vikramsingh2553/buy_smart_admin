60eff35efa8c68684ee1513da815d6770b0f45ae
amitpaju0001 <amitpaju0001@gmail.com>
2024-07-24 16:38:38 +0530

    merge files

---
+++ lib/Product/ui/product_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ProductScreen extends StatefulWidget {
+  const ProductScreen({super.key});
+
+  @override
+  State<ProductScreen> createState() => _ProductScreenState();
+}
+
+class _ProductScreenState extends State<ProductScreen> {
+  final List<String> products = List.generate(10, (index) => 'Product $index');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Products'),
+      ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 2 / 1,
+        ),
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          return Card(
+            margin: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Text(products[index]),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
