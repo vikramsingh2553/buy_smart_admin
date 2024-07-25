6429b10c2be84b934ea7567b000399bd9316386a
amitpaju0001 <amitpaju0001@gmail.com>
2024-07-25 17:15:05 +0530

    implement add products in add products screen

--- lib/Product/ui/add_products_screen.dart
+++ lib/Product/ui/add_products_screen.dart
@@ -1,5 +1,8 @@
 import 'package:buy_smart_admin/Product/ui/product_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:buy_smart_admin/Product/model/product_model.dart';
+import 'package:buy_smart_admin/Product/provider/product_provider.dart';
 
 class AddProductsScreen extends StatefulWidget {
   const AddProductsScreen({super.key});
@@ -12,6 +15,9 @@ class _AddProductsScreenState extends State<AddProductsScreen> {
   final _formKey = GlobalKey<FormState>();
   String? _selectedCategory;
   final List<String> _categories = ['Electronics', 'Fashion', 'Home', 'Beauty', 'Sports'];
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _priceController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -46,6 +52,7 @@ class _AddProductsScreenState extends State<AddProductsScreen> {
               ),
               const SizedBox(height: 16),
               TextFormField(
+                controller: _nameController,
                 decoration: const InputDecoration(
                   labelText: 'Product Name',
                   border: OutlineInputBorder(),
@@ -59,6 +66,7 @@ class _AddProductsScreenState extends State<AddProductsScreen> {
               ),
               const SizedBox(height: 16),
               TextFormField(
+                controller: _priceController,
                 decoration: const InputDecoration(
                   labelText: 'Price',
                   border: OutlineInputBorder(),
@@ -73,6 +81,7 @@ class _AddProductsScreenState extends State<AddProductsScreen> {
               ),
               const SizedBox(height: 16),
               TextFormField(
+                controller: _descriptionController,
                 decoration: const InputDecoration(
                   labelText: 'Description',
                   border: OutlineInputBorder(),
@@ -87,8 +96,15 @@ class _AddProductsScreenState extends State<AddProductsScreen> {
               ),
               const SizedBox(height: 16),
               ElevatedButton(
-                onPressed: () {
+                onPressed: () async {
                   if (_formKey.currentState?.validate() ?? false) {
+                    final product = ProductModel(
+                      id: '',
+                      name: _nameController.text,
+                      description: _descriptionController.text,
+                      price: int.parse(_priceController.text),
+                    );
+                    await Provider.of<ProductProvider>(context, listen: false).addProduct(product);
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(content: Text('Product added successfully')),
                     );
